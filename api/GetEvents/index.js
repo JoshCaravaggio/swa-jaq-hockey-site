ee2058153205f14c535d3d38cae32bee408d28c2
Josh Caravaggio <josh@Joshs-MacBook-Pro.local>
2021-07-11 10:21:59 -0400

    dynamic sessions page

--- api/GetEvents/index.js
+++ api/GetEvents/index.js
@@ -1,29 +1,45 @@
 const azure = require('azure-storage');
 
 module.exports = async function (context, req) {
-    // context.log(`COnnection string: ${process.env.TableStorage}`);
 
     const tableService = azure.createTableService(process.env.TableStorage);
     const tableName = "Events";
 
-
-    const events = await executeQuery();
-    context.res = {
-        status: 200,
-        body: events
+    try {
+        const events = await executeQuery();
+        context.res = {
+            status: 200,
+            body: events
+        }
     }
-
-    context.log("Done")
+    catch (err) {
+        context.log(err);
+        context.res = {
+            status: 500,
+            body: "An error occured while getting the events."
+        }
+    }
 }
+
 async function executeQuery() {
     try {
         const tableService = azure.createTableService(process.env.TableStorage);
         return new Promise((resolve, reject) => {
             var query = new azure.TableQuery().top(100);
             tableService.queryEntities("Events", query, null, function (error, result, response) {
-                //console.log(response)
+                console.log(response)
                 if (!error) {
-                    resolve(result.entries);
+                    var events = [];
+                    result.entries.forEach(element => {
+                        events.push({
+                            startTimestamp: element.StartTime._,
+                            additionalInfo: element.AdditionalInfo._,
+                            location: element.Location._,
+                            dateTimeDescription: element.DateTimeDescription._
+                        });
+                    })
+                    events.sort(compareEventTimes);
+                    resolve(events);
                 } else {
                     resolve(error);
                 }
@@ -35,4 +51,14 @@ async function executeQuery() {
             reject(e);
         });
     }
+}
+
+function compareEventTimes(a, b) {
+    if (a.startTimestamp < b.startTimestamp) {
+        return -1;
+    }
+    if (a.startTimestamp > b.startTimestamp) {
+        return 1;
+    }
+    return 0;
 }
---
+++ pregen/events.js
@@ -0,0 +1,56 @@
+const LOCAL_BASE_URL = 'http://localhost:7071';
+const AZURE_BASE_URL = 'https://orange-beach-0331eec10.azurestaticapps.net/api/Getevents';
+
+$(document).ready(function () {
+    $.ajax({
+        url: "http://127.0.0.1:7071/api/GetEvents",
+        error: function (err) {
+            document.getElementById("SessionsContainer").innerHTML += renderSession(element);
+        }
+    }).then(function (data) {
+        console.log(data)
+
+        if (data.length === 0) {
+            document.getElementById("SessionsContainer").innerHTML += renderSession(element);
+        }
+        JSON.parse(data).forEach(element => {
+            document.getElementById("SessionsContainer").innerHTML += renderSession(element);
+        });
+    });
+});
+
+
+const getAPIBaseUrl = () => {
+    const isLocal = /localhost/.test(window.location.href);
+    return isLocal ? LOCAL_BASE_URL : AZURE_BASE_URL;
+}
+
+const renderSession = (event) => {
+
+    var sessionHTML = `<div class="event-container">
+    <br>
+        <div class="row">
+            <div class="col-md-4 col-sm-12 session-title"><b>Date &amp; Time </b></div> 
+            <div class="col-md-8 col-sm-12 session-datetime">${event.dateTimeDescription}</div> 
+        </div>   
+    <div class="row">
+        <div class="col-md-4 col-sm-12 session-price"><b>Location</b></div> 
+        <div class="col-md-8 col-sm-12 session-location">${event.location}</div> 
+    </div>   
+    <div class="row">
+    <div class="col-md-4 col-sm-12 session-price"><b>Additional Info</b></div> 
+    <div class="col-md-8 col-sm-12 session-location">${event.additionalInfo}</div> 
+    </div>   
+    <br>
+    <div class="row">
+    <div class="col-12" style="text-align:center">
+        <a href="mailto:jaqhockey@gmail.com?subject=RSVP for On-Ice Session: ${event.dateTimeDescription}}">
+        <div class="session-rsvp" style="padding-left:30px;padding-right:30px;">RSVP</div>
+        </a>
+    </div>
+    </div>
+    <br>
+    </div>`
+    return sessionHTML;
+
+}
