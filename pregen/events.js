2bae166a8a23263725d6c2abeff8c0c3ad6eccff
Josh Caravaggio <josh@joshs-mbp.local>
2021-07-21 17:55:10 -0400

    added loading spinner

--- pregen/events.js
+++ pregen/events.js
@@ -35,6 +35,7 @@ $(document).ready(function () {
                 document.getElementById("SessionsContainer").innerHTML += renderSession(element, eventType);
             });
         }
+        $("#loadingSpinner").hide();
     });
 });
 
